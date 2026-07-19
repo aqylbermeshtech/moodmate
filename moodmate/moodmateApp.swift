b9e767d7f035459283c3e1e2a6105463144ecb17
Nurtore Kambar <nurtorekambar21@gmail.com>
2026-07-19 23:52:25 +0500

    Initial Commit

---
+++ moodmate/moodmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodmateApp.swift
+//  moodmate
+//
+//  Created by Nurtore on 19.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct moodmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
